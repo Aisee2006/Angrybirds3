65aa96aa400d928b5f980c75a0bfe186c457d4a5
Aisee <chumur@gmail.com>
2020-07-26 17:57:52 +0530

    added and modified

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var log6,slingshot;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,17 +34,18 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    log6 = new Log(230,180,80,PI/2);
 
     bird = new Bird(100,100);
+    slingshot = new Slingshot(bird.body,log6.body);
 
+   
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    
     box1.display();
     box2.display();
     ground.display();
@@ -59,6 +61,11 @@ function draw(){
     log4.display();
     log5.display();
 
+
     bird.display();
     platform.display();
+    log6.display();
+    slingshot.display();
+  
+
 }
